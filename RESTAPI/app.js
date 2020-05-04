751962fc5dcb681aee9f4bea97100e4ee8b6b15b
8180223 <8180223@estg.ipp.pt>
2020-05-04 16:08:14 +0100

    DB connection, routers changes

--- RESTAPI/app.js
+++ RESTAPI/app.js
@@ -4,8 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const apiRouter = require('./routes/api');
 
 const app = express();
 
@@ -15,13 +14,9 @@ const swaggerDocumment = require('./swagger.json');
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/products-demo', {useNewUrlParser: true , useUnifiedTopology: true})
+mongoose.connect('mongodb+srv://admin:pawtrabalhopaw2020@cluster0-6uaiy.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true , useUnifiedTopology: true})
   .then(() =>  console.log('connection succesful'))
   .catch((err) => console.error(err));
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -29,11 +24,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/v1', apiRouter);
 
 app.use('/api-docs',swaggerUi.serve , swaggerUi.setup(swaggerDocumment));
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
