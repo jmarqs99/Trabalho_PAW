37f8cd7188a217eeb3e7852e9c41aeb72bc94043
Miguel <migueltomaz.c@gmail.com>
2020-05-05 17:42:19 +0100

    swagger pedido 1.1

--- RESTAPI/routes/api/pedido.js
+++ RESTAPI/routes/api/pedido.js
@@ -2,11 +2,7 @@ const express = require('express');
 const router = express.Router();
 const pedidoController = require("../../controllers/pedidoController");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send({working: true})
-});
-router.get('/pedidos', pedidoController.getAllPedidos);
+router.get('/', pedidoController.getAllPedidos);
 router.get('/:pedidoId', pedidoController.getOnePedido);
 
 router.post('/', pedidoController.criarPedido);
