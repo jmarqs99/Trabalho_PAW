907b7b5d7cb709460fd6b7682d7eda84fb73cdb3
8180223 <8180223@estg.ipp.pt>
2020-05-07 20:08:37 +0100

    Midlleware

---
+++ RESTAPI/middlewares/session.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken')
+
+process.env.JWT_SECRET = 'this is for development'
+
+const sessionMiddleware = (req, res, next) => {
+	const sessionStr = req.cookies.session
+	try {
+		if (sessionStr) {
+			const user = jwt.verify(sessionStr, process.env.JWT_SECRET)
+			req.user = user
+		} else {
+			req.user = null
+		}
+	} catch(e) {
+		req.user = null
+	}
+	next()
+}
+
+module.exports = sessionMiddleware
