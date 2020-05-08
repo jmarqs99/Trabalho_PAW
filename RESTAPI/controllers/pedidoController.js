8cada56631cde1c813ca9be47ce81ff633d545c8
Miguel <migueltomaz.c@gmail.com>
2020-05-08 22:19:44 +0100

    Filtrar pedidos
    (listar pedidos com o estado do user ainda n funciona)

--- RESTAPI/controllers/pedidoController.js
+++ RESTAPI/controllers/pedidoController.js
@@ -20,10 +20,25 @@ pedidoController.getAllPedidos = function(req, res, next) {
         if(err) {
             next(err)
         }else {
+           
+
+            
+            res.json(pedidos)
+            
+        }
+    })
+}
+
+pedidoController.numeroInfetados = function(req, res, next) {
+    Pedido.find(function(err, pedidos){
+        if(err) {
+            next(err)
+        }else {
             res.json(pedidos)
         }
     })
 }
+
 
 pedidoController.getOnePedido = function(req, res, next) {
     
@@ -38,6 +53,175 @@ pedidoController.getOnePedido = function(req, res, next) {
     })
     
 }
+pedidoController.allResultadoPositivoPedido = function(req, res, next) {
+
+    //Pedido.findOne({_resultadoTeste:req.params.positivo}, function(err, pedido){
+
+        Pedido.find({resultadoTeste:req.params.positivo}, function(err, pedido){
+            if(err) {
+                next(err)
+            }
+            else {
+                res.json(pedido)
+                
+            }
+        })
+
+}
+pedidoController.allResultadoNegativoPedido = function(req, res, next) {
+
+
+        Pedido.find({resultadoTeste:req.params.negativo}, function(err, pedido){
+            if(err) {
+                next(err)
+            }
+            else {
+                res.json(pedido)
+                
+            }
+        })
+
+}
+
+pedidoController.allEstadoTesteFinalizadoPedido = function(req, res, next) {
+
+    //Pedido.findOne({_resultadoTeste:req.params.positivo}, function(err, pedido){
+
+        Pedido.find({estadoTeste:req.params.finalizado}, function(err, pedido){
+            if(err) {
+                next(err)
+            }
+            else {
+                res.json(pedido)
+                
+            }
+        })
+
+}
+
+pedidoController.allEstadoTestePendentePedido = function(req, res, next) {
+
+    //Pedido.findOne({_resultadoTeste:req.params.positivo}, function(err, pedido){
+
+        Pedido.find({estadoTeste:req.params.pendente}, function(err, pedido){
+            if(err) {
+                next(err)
+            }
+            else {
+                res.json(pedido)
+                
+            }
+        })
+
+}
+
+pedidoController.allEstadoUserDoentePedido = function(req, res, next) {
+
+    //Pedido.findOne({_resultadoTeste:req.params.positivo}, function(err, pedido){
+
+        Pedido.find({estadoTeste:req.params.doente}, function(err, pedido){
+            if(err) {
+                next(err)
+            }
+            else {
+                res.json(pedido)
+                
+            }
+        })
+
+}
+pedidoController.allEstadoUserSaudavelPedido = function(req, res, next) {
+
+    //Pedido.findOne({_resultadoTeste:req.params.positivo}, function(err, pedido){
+
+        Pedido.find({estadoTeste:req.params.saudavel}, function(err, pedido){
+            if(err) {
+                next(err)
+            }
+            else {
+                res.json(pedido)
+                
+            }
+        })
+
+}
+
+pedidoController.getOnePedidoByEstadoTeste = async function (req,res,next) {
+
+    
+    Pedido.findOne({_id:req.params.pedidoId},function (err,pedido)  {
+        if(err) {
+            next(err);
+        } else {
+          
+                    const result = pedido.toObject();
+                    result.informacao = Pedido.informacao
+                    result.estadoUtilizador = Pedido.estadoUtilizador
+                    result.resultadoTeste = Pedido.resultadoTeste
+                    
+                    res.json(result);
+                
+                
+           
+        }
+     });
+};
+pedidoController.getOnePedidoByResultado = async function (req,res,next) {
+    Pedido.findOne({_id:req.params.pedidoId},function (err,pedido)  {
+        if(err) {
+            next(err);
+        } else {
+          
+                    const result = pedido.toObject();
+                    result.informacao = Pedido.informacao
+                    result.estadoUtilizador = Pedido.estadoUtilizador
+                    result.estadoTeste = Pedido.estadoTeste
+                    
+                    res.json(result);
+                
+                
+           
+        }
+     });
+};
+
+pedidoController.getOnePedidoByEstadoUtilizador = async function (req,res,next) {
+    Pedido.findOne({_id:req.params.pedidoId},function (err,pedido)  {
+        if(err) {
+            next(err);
+        } else {
+          
+                    const result = pedido.toObject();
+                    result.informacao = Pedido.informacao
+                    result.resultadoTeste = Pedido.resultadoTeste
+                    result.estadoTeste = Pedido.estadoTeste
+                    
+                    res.json(result);
+                
+                
+           
+        }
+     });
+};
+
+pedidoController.getOnePedidoByInformacao = async function (req,res,next) {
+    Pedido.findOne({_id:req.params.pedidoId},function (err,pedido)  {
+        if(err) {
+            next(err);
+        } else {
+          
+                    const result = pedido.toObject();
+                    result.estadoUtilizador = Pedido.estadoUtilizador
+                    result.resultadoTeste = Pedido.resultadoTeste
+                    result.estadoTeste = Pedido.estadoTeste
+                    
+                    res.json(result);
+                
+                
+           
+        }
+     });
+};
 
 pedidoController.updatePedido = function(req, res, next) {
 
@@ -50,5 +234,8 @@ pedidoController.updatePedido = function(req, res, next) {
             }
         });
 }
+
+
+
 
 module.exports = pedidoController;
--- RESTAPI/routes/api/pedido.js
+++ RESTAPI/routes/api/pedido.js
@@ -4,9 +4,25 @@ const pedidoController = require("../../controllers/pedidoController");
 
 router.get('/', pedidoController.getAllPedidos);
 router.get('/:pedidoId', pedidoController.getOnePedido);
+router.get('/estadoTeste/:pedidoId', pedidoController.getOnePedidoByEstadoTeste)
+router.get('/resultado/:pedidoId', pedidoController.getOnePedidoByResultado)
+router.get('/estadoUser/:pedidoId', pedidoController.getOnePedidoByEstadoUtilizador)
+router.get('/informacao/:pedidoId', pedidoController.getOnePedidoByInformacao)
+//router.get('/numero/', pedidoController.numeroInfetados)
+
+router.get('/resultadoTeste/:positivo', pedidoController.allResultadoPositivoPedido)
+router.get('/resultadoTeste/:negativo', pedidoController.allResultadoNegativoPedido)
+
+
+router.get('/estado/:finalizado', pedidoController.allEstadoTesteFinalizadoPedido)
+router.get('/estado/:pendente', pedidoController.allEstadoTestePendentePedido)
+
+router.get('/estadoUtilizador/:doente', pedidoController.allEstadoUserDoentePedido)
+router.get('/estadoUtilizador/:saudavel', pedidoController.allEstadoUserSaudavelPedido)
 
 router.post('/', pedidoController.criarPedido);
 router.put('/:pedidoId', pedidoController.updatePedido);
+
 
 //router.param('pedidoId', pedidoController.getByIdProduct)
 
