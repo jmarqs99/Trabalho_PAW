db7a437ba2521f7e6f34583d23815bca41859da2
jmarqs99 <8180551@estg.ipp.pt>
2020-05-27 16:43:02 +0100

    user select

--- FRONTEND/src/app/utilizadores/utilizadores.component.ts
+++ FRONTEND/src/app/utilizadores/utilizadores.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RestService } from '../rest.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import {Utilizador} from '../Models/Utilizador';
+import { Utilizador } from '../Models/Utilizador';
 import { from } from 'rxjs';
 
 @Component({
@@ -11,61 +11,74 @@ import { from } from 'rxjs';
 })
 export class UtilizadoresComponent implements OnInit {
 
-  utilizadores:any=[]; 
-  utilizador:any;
-  currentUtilizador : any;
+  utilizadores: any = [];
+  utilizador: any;
+  currentUtilizador: any;
   viewingUtilizador: boolean;
   atualizar: boolean;
-  atualizarUtilizador:boolean;
+  atualizarUtilizador: boolean;
   novoEstado: String;
 
 
-  constructor(public rest:RestService, private route: ActivatedRoute, private router: Router) {}
+  constructor(public rest: RestService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     this.getUtilizadores();
   }
 
 
-  getUtilizadores(){
+  getUtilizadores() {
     this.utilizadores = [];
-    this.rest.getUtilizadores().subscribe((data:{})=>{
+    this.rest.getUtilizadores().subscribe((data: {}) => {
       this.utilizadores = data;
     });
   }
 
-  getUtilizador(_id:String){
-    this.utilizador=null;
-    this.rest.getUtilizador(_id).subscribe((data:{})=>{
-      this.utilizador=data;
+  getUtilizador(_id: String) {
+    this.utilizador = null;
+    this.rest.getUtilizador(_id).subscribe((data: {}) => {
+      this.utilizador = data;
     })
   }
 
-  utilizadorInfo(Id :string){
+  utilizadorInfo(Id: string) {
     if (this.viewingUtilizador) {
       this.viewingUtilizador = false;
     } else {
-      this.rest.getUtilizador(Id).subscribe((data:{})=>{
-        this.currentUtilizador = data;
-        this.viewingUtilizador = true;
+      let utilizadorResult: any = null;
+      new Promise((resolve, reject) => {
+        const utilizadores = this.utilizadores;
+        utilizadores.forEach(function (utilizador, index) {
+          if (utilizador._id == Id) {
+            utilizadorResult = utilizador;
+            resolve();
+          }
+          if (index === utilizadores.length - 1) resolve();
+        });
+
+      }).then(() => {
+        this.currentUtilizador = utilizadorResult;
+        this.viewingUtilizador = true
       });
     }
   }
+
+
   update() {
-    this.rest.updateUtilizador(this.currentUtilizador._id,this.novoEstado)
-    .subscribe(res => {
-      this.atualizar = false;
-      this.getUtilizadores();
-      this.viewingUtilizador = false;
-      this.utilizadorInfo(this.currentUtilizador._id);
-    }, (err) => {
-      console.log(err);
-    }
-    );
+    this.rest.updateUtilizador(this.currentUtilizador._id, this.novoEstado)
+      .subscribe(res => {
+        this.atualizar = false;
+        this.getUtilizadores();
+        this.viewingUtilizador = false;
+        this.utilizadorInfo(this.currentUtilizador._id);
+      }, (err) => {
+        console.log(err);
+      }
+      );
   }
- /*  updateUtilizador(Id :string, dados:JSON){
-    this.rest.updateUtilizador(this.route.snapshot.params['id'],dados)
-  } */
-  
+  /*  updateUtilizador(Id :string, dados:JSON){
+     this.rest.updateUtilizador(this.route.snapshot.params['id'],dados)
+   } */
+
 
 }
