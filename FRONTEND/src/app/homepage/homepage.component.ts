d4cc31c8c5b7e6e8e31aa64e6d7527e5706976f3
jmarqs99 <8180551@estg.ipp.pt>
2020-05-28 19:19:48 +0100

    home

--- FRONTEND/src/app/homepage/homepage.component.ts
+++ FRONTEND/src/app/homepage/homepage.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RestService } from '../rest.service';
 import { Router } from '@angular/router';
 
+
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
