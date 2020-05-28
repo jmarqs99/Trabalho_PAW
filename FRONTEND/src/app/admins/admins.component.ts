652d65a14ce1709a421d1236c982dc6e7ae0d3f3
8180223 <8180223@estg.ipp.pt>
2020-05-28 20:39:07 +0100

    Admins

--- FRONTEND/src/app/admins/admins.component.ts
+++ FRONTEND/src/app/admins/admins.component.ts
@@ -8,63 +8,61 @@ import { Router } from '@angular/router';
   styleUrls: ['./admins.component.css']
 })
 export class AdminsComponent implements OnInit {
-  admins : any;
+  admins: any;
   addingAdmin: boolean;
   userId: string;
-  currentAdmin : any;
+  currentAdmin: any;
   viewingAdmin: boolean;
 
-  constructor(private router: Router,public rest:RestService) { }
+  constructor(private router: Router, public rest: RestService) { }
 
   ngOnInit(): void {
     this.getAdmins();
   }
 
-  getAdmins(){
+  getAdmins() {
     this.admins = [];
-    this.rest.getAdmins().subscribe((data:{})=>{
+    this.rest.getAdmins().subscribe((data: {}) => {
       this.admins = data;
       console.log(data)
     });
   }
 
-  demote(tecnicoId : string){
+  demote(tecnicoId: string) {
     var doRemove = confirm("Queres mesmo remover este técnico?");
     if (doRemove == true) {
-      this.rest.removeAdmin(tecnicoId).subscribe((data:{})=>{
+      this.rest.removeAdmin(tecnicoId).subscribe((data: {}) => {
         this.getAdmins();
       });
     }
-    
+
   }
 
-  addAdmin(){
-    if (this.userId == null || this.userId == ''){
+  addAdmin() {
+    if (this.userId == null || this.userId == '') {
       alert("ID de utilizador inválido!")
       return;
     }
-    this.rest.addAdmin(this.userId).subscribe((data:{})=>{
+    this.rest.addAdmin(this.userId).subscribe((data: {}) => {
       this.getAdmins();
       this.addingAdmin = false;
       this.userId = null;
     });
   }
-  adminInfo(tecnicoId :string){
-    if (this.viewingAdmin) {
+  adminInfo(adminId: string) {
+    if (this.viewingAdmin && this.currentAdmin._id == adminId) {
       this.viewingAdmin = false;
     } else {
-      var adminResult:any = null;
+      var adminResult: any = null;
       new Promise((resolve, reject) => {
-      const admins = this.admins;
-      admins.forEach(function(admin,index){
-        if (admin._id == tecnicoId){
-          adminResult = admin;
-          this.currentAdmin = admin;
-          this.viewingAdmin = true
-          resolve();
-        }
-        if (index === admins.length -1) resolve();
-      });
+        const admins = this.admins;
+        admins.forEach(function (admin, index) {
+          if (admin._id == adminId) {
+            adminResult = admin;
+            resolve();
+          }
+          if (index === admins.length - 1) resolve();
+        });
       }).then(() => {
         this.currentAdmin = adminResult;
         this.viewingAdmin = true
